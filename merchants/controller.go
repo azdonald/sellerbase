c60bb76bea94af6df15ef5eb28af6b53742da5ac
Azu <azdonald@yahoo.co.uk>
2020-11-01 22:35:30 -0500

    testing

--- merchants/controller.go
+++ merchants/controller.go
@@ -11,7 +11,7 @@ func New() *restful.WebService {
 	fmt.Print("merchant service")
 	service := new(restful.WebService)
 	service.Path("merchants").Consumes(restful.MIME_JSON).Produces(restful.MIME_JSON)
-	service.Route(service.GET("/{merchants}").To(FindMerchant))
+	service.Route(service.GET("").To(FindMerchant))
 	return service
 }
 
