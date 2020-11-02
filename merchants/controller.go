f1a8b74e41ae2590002a4a8a0737296ab21b4c78
Azu <azdonald@yahoo.co.uk>
2020-11-02 14:36:24 -0500

    Add missing slash to route

--- merchants/controller.go
+++ merchants/controller.go
@@ -11,7 +11,7 @@ import (
 func New() *restful.WebService {
 	fmt.Print("merchant service")
 	service := new(restful.WebService)
-	service.Path("merchants").Consumes(restful.MIME_JSON).Produces(restful.MIME_JSON)
+	service.Path("/merchants").Consumes(restful.MIME_JSON).Produces(restful.MIME_JSON)
 	service.Route(service.GET("/{id}").To(FindMerchant))
 	service.Route(service.POST("").To(CreateMerchant))
 	return service
