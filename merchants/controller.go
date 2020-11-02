29967a981a808c732672186084f64f4e7015345d
Azu <azdonald@yahoo.co.uk>
2020-11-02 14:26:23 -0500

    Added base struct and merchant model

--- merchants/controller.go
+++ merchants/controller.go
@@ -2,6 +2,7 @@ package merchants
 
 import (
 	"fmt"
+	"net/http"
 
 	restful "github.com/emicklei/go-restful/v3"
 )
@@ -10,12 +11,23 @@ import (
 func New() *restful.WebService {
 	fmt.Print("merchant service")
 	service := new(restful.WebService)
-	service.Path("/merchants").Produces(restful.MIME_JSON)
-	service.Route(service.GET("").To(FindMerchant))
+	service.Path("merchants").Consumes(restful.MIME_JSON).Produces(restful.MIME_JSON)
+	service.Route(service.GET("/{id}").To(FindMerchant))
+	service.Route(service.POST("").To(CreateMerchant))
 	return service
 }
 
 //FindMerchant returns a merchant
 func FindMerchant(req *restful.Request, res *restful.Response) {
 	res.WriteEntity("Hello")
+}
+
+// CreateMerchant is handler for creating a new merchant
+func CreateMerchant(req *restful.Request, res *restful.Response) {
+	merchant := new(Merchant)
+	err := req.ReadEntity(&merchant)
+	if err != nil {
+		res.WriteError(http.StatusInternalServerError, err)
+	}
+	res.WriteEntity(merchant)
 }
